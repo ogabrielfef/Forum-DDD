789be5ff61d9b2cb6d6edafbf14e7d71be5e25ae
ogabrielfef <ogabrielfef@gmail.com>
2025-01-08 23:41:45 -0300

    Aplicando o conceito de value-objects

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,29 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Method to create a slug using title/string
+   * Receives a string and normalize it as a slug
+   * 
+   * Example: "An example title" => "an-example-title"
+   * 
+   * @param text {string}
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize("NFKD")
+      .toLocaleLowerCase()
+      .trim()
+      .replace(/\s+/g, '-') // Regex for remove white spaces in the string
+      .replace(/[^\w-]+/g, '') // Regex for remove every that is not words
+      .replace(/_/g, '-') // Regex to replace '_' for '-'
+      .replace(/--+/g, '-') // Regex to replace '--' for '-'
+      .replace(/-$/g, '') // Regex to remove one '-' if there are two at the end of the string
+
+    return new Slug(slugText)
+  }
+}
