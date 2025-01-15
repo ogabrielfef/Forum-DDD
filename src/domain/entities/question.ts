322a647e56b1d8922e8a36f4a6bda74c1e93f242
ogabrielfef <ogabrielfef@gmail.com>
2025-01-14 21:28:08 -0300

    Estudos DDD: criação da pasta core e entidades base(pai), refatoração das entidades em value-objects.

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,25 +1,16 @@
-import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
+import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
+	authorId: UniqueEntityId
+	bestAnswerId?: UniqueEntityId
 	title: string
 	content: string
 	slug: Slug
-	authorId: string
+	createdAt: Date
+  updatedAt?: Date
 }
 
-export class Question {
-	public id: string
-	public title: string
-	public slug: Slug
-	public content: string
-	public authorId: string
-
-	constructor(props: QuestionProps, id?: string) {
-		this.title = props.title
-		this.content = props.content
-		this.slug = props.slug
-		this.authorId = props.authorId
-		this.id = id ?? randomUUID()
-	}
+export class Question extends Entity<QuestionProps> {
 }
